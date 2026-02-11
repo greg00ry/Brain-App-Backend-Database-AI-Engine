1fa437b3a8961a25419b56342d8359f8c1783899
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-02-11 21:38:51 +0100

    Update RegisterForm.tsx

--- frontend/src/components/RegisterForm.tsx
+++ frontend/src/components/RegisterForm.tsx
@@ -1,15 +1,58 @@
+import React, { useState } from "react";
+import type { ChangeEvent } from 'react';
 
+const RegisterForm: React.FC = () => {
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [repeatPassword, setReapeatPassword] = useState<string>("")
 
+    //Typowanie zdarzenia zmiany inputa
+    const handleChange = (e: ChangeEvent<HTMLInputElement>, setter: React.Dispatch<React.SetStateAction<string>>) => {
+        setter(e.target.value)
+    }
 
-function RegisterForm() {
+    //Typowanie zdarzenia wysyłki arkusza
+    const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        
+        if (password !== repeatPassword) {
+            alert("Hasła nie są identyczne")
+            return
+        }
+
+        console.log("TSX Log: Wysyłka danych", {email, password})
+        //tutaj bedzie logika rejestracji
+
+    }
+
     return (
         <div>
-            <form action="/register" method="post">
-                Podaj adres email: <input type="text" />
-                Podaj hasło: <input type="password" />
-                Powtórz hasło: <input type="password" />
-                <button type="submit">Zarejestruj się</button>
+            <h2>Zarejestruj się</h2>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email"
+                    placeholder="Email"
+                    required
+                    onChange={(e) => handleChange(e, setEmail)}    
+                />
+                <input 
+                    type="password"
+                    placeholder="Hasło"
+                    required
+                    onChange={(e)=> handleChange(e, setPassword)}
+                />
+                <input 
+                    type="password"
+                    placeholder="Powtórz hasło"
+                    required
+                    onChange={(e) => handleChange(e, setReapeatPassword)}
+                />
+                <button type="submit">
+                    Utwórz konto
+                </button>
             </form>
         </div>
     )
 }
+
+export {RegisterForm}
