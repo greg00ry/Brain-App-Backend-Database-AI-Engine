2b49530509bb8947bf3b7cd70af4ffc9a801403c
greg00ry <grzegorz.trzaskoma21@gmail.com>
2026-02-12 19:59:44 +0100

    Corrected typos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ function App() {
             <div className="bg-white rounded-lg p-8 shadow-lg max-w-md w-full">
               <button
                   onClick={handleCloseLoginModal}
-                  className='ansolute top-4 right-4 text-gray-500 hover:text-gray-800'
+                  className='absolute top-4 right-4 text-gray-500 hover:text-gray-800'
                 >X</button>
                 <h2 className='text-xl font-bold mb-4'>Zaloguj się</h2>
               <LogginForm/>
